38727a68bbe9d679d036b9f90829a9acbbb81ae8
Abhinai <abhinaisai10@gmail.com>
2022-07-30 12:43:16 +0530

    added fetch function

--- src/utils/FetchAPI.js
+++ src/utils/FetchAPI.js
@@ -1,2 +1,8 @@
-// This is a global function using 
 //This is a global function using 
+//This is a global function using 
+
+const sendReq = (url)=>{
+    fetch(url).then(res => res.json()).then(res => console.log(res));
+}
+
+sendReq("https://jsonplaceholder.typicode.com/posts");
