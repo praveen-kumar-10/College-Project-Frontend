c830ad4228b71a21ae79946bedb2a1ccd6ab6a23
N Praveen Kumar <praveen@Ns-MacBook-Air.local>
2022-07-30 12:29:59 +0530

    Button Component Added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import "./App.css";
+import Button from "./components/UI/Button";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Button
+        className="btn-primary"
+        title="Button"
+        type="button"
+        onClick={() => alert("hello")}
+      />
+    </div>
+  );
 }
 
 export default App;
